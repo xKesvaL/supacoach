32ced8d5f871541fd8b32e76e085bdb0647c209b
xKesvaL <jabeddou@gmail.com>
2024-10-02 22:59:33 -0400

    feat: add cors

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,17 @@
 import { Hono } from "hono";
 import { auth } from "./lib/auth";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(
+	cors({
+		origin: ["http://localhost:5173"],
+		allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+		allowHeaders: ["Content-Type", "Authorization"],
+		credentials: true,
+	}),
+);
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
