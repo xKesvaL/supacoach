e3c8e6ae9377183d21e70f279733e3489da82081
xKesvaL <jabeddou@gmail.com>
2024-11-03 19:32:46 +0000

    chore: setup project

---
+++ supabase/README.md
@@ -0,0 +1,11 @@
+# Supabase Docker
+
+This is a minimal Docker Compose setup for self-hosting Supabase. Follow the steps [here](https://supabase.com/docs/guides/hosting/docker) to get started.
+
+## Update env variables
+
+After the `.env` file is updated, run the following command to apply the changes:
+
+```bash
+docker-compose up -d
+```
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import { paraglide } from '@inlang/paraglide-sveltekit/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+
+export default defineConfig({
+	plugins: [
+		sveltekit(),
+		paraglide({
+			project: './project.inlang',
+			outdir: './src/lib/paraglide'
+		})
+	],
+
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
